549e6b92de55327d79cee27033c56f56c0e53957
bruno <bruno_sadoski@hotmail.com>
2020-11-04 16:04:18 -0300

    aula 2 soluçoes

--- aula2/exercicio_dois.py
+++ aula2/exercicio_dois.py
@@ -1,10 +1,27 @@
+# Crie uma lista de compras com 10 produtos e seus preços
+compras = {
+    "arroz" : 30,
+    "feijão" : 10,
+    "macarrão": 5,
+    "batata": 3,
+    "trigo": 12,
+    "oleo": 7,
+    "picanha": 50
+}
+print(compras)
 
 # Calcule o valor total dos itens e aplique descontos:
-
+total = sum(compras.values())
+print(total)
+if (total > 100):
+    print(total-(total*0.02))
+elif (total > 200):
+    print(total-(total*0.05))
+elif (total > 500):
+    print(total-(total*0.1))
 # se o valor for superior a 100, 2% de desconto
 # se o valor for superior a 200, 5% de desconto
 # se o valor for superior a 500, 10% de desconto
 
 # descubra qual o item mais caro da lista
-
-# ordene os itens da lista por ordem alfabética
+print(max(compras, key=compras.get))
